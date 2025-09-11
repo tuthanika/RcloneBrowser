0e634f7a67acae0f2695678a9d02e06f11b370ef
tuthanika <tuthanika@gmail.com>
2025-09-11 22:19:57 +0700

    Revert "fixx"
    This reverts commit 9b7cf7597af682a7efdeaebc7be23b622133fb54.

--- src/transfer_dialog.cpp
+++ src/transfer_dialog.cpp
@@ -823,6 +823,14 @@ QString TransferDialog::getDest() const {
   return ui.textDest->text();
 }
 
+QString TransferDialog::getDest() const {
+  if (mIsDownload) {
+    return ui.textDest->text();
+  } else {
+    return mRemote + ":" + ui.textDest->text();
+  }
+}
+
 QStringList TransferDialog::getOptions() {
   JobOptions *jobo = getJobOptions(mJobOptions);
   QStringList newWay = jobo->getOptions();
